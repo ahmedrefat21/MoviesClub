14ddf490f902420b82ac8a38cee33c729d7dd72a
ahmedrefat21 <ahmedref21@gmail.com>
2024-08-24 23:50:10 +0300

    Initial commit

---
+++ MoviesClub/MoviesClubApp.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesClubApp.swift
+//  MoviesClub
+//
+//  Created by Ahmed Refat on 24/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesClubApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
