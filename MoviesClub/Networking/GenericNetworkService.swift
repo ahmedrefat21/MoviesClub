f2dbbf9f82f9d2f0b70effc09efc620a8a168359
ahmedrefat21 <ahmedref21@gmail.com>
2024-08-25 00:30:33 +0300

    add generic method that fetch data

---
+++ MoviesClub/Networking/GenericNetworkService.swift
@@ -0,0 +1,49 @@
+//
+//  GenericNetworkService.swift
+//  MoviesClub
+//
+//  Created by Ahmed Refat on 25/08/2024.
+//
+
+import Foundation
+
+struct GenericNetworkService {
+    
+    private static let session: URLSession = URLSession(configuration: URLSessionConfiguration.default)
+    
+    static func getData<T:Codable>(from url: URL,completion: @escaping(Result<T,CSError>) -> Void) {
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request) { data, response, error in
+            
+            if let anyError = error {
+                print(anyError.localizedDescription)
+                completion(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse ,(200...299).contains(response.statusCode) else {
+                print("Error in response")
+                completion(.failure(.invailedResponse))
+                return
+            }
+            
+            guard let data = data else {
+                print("Error in data")
+                completion(.failure(.invailedData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(T.self, from: data)
+                completion(.success(result))
+            } catch (let error) {
+                print("Cache error \(error.localizedDescription)")
+                completion(.failure(.invailedData))
+            }
+        }
+        task.resume()
+        
+    }
+    
+}
---
+++ MoviesClub/Utilities/CSError.swift
@@ -0,0 +1,16 @@
+//
+//  CSError.swift
+//  MoviesClub
+//
+//  Created by Ahmed Refat on 25/08/2024.
+//
+
+import Foundation
+
+enum CSError : String,Error {
+    
+    case unableToComplete = "Unable to completed your request. Please check your internet connection."
+    case invailedResponse = "Invalid response from the server.Please try again"
+    case invailedData = "The data received from the server was invailed. Please try again."
+}
+
