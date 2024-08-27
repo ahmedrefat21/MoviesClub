f64f88515369cc4b948bb977c45a6e9d5454dccc
ahmedrefat21 <ahmedref21@gmail.com>
2024-08-27 13:45:29 +0300

    handle the optional value of url with a guard let

--- MoviesClub/Networking/MovieURL.swift
+++ MoviesClub/Networking/MovieURL.swift
@@ -14,23 +14,27 @@ struct MovieURL {
     private static let baseImageURLString = "https://image.tmdb.org/t/p/w500/"
     private static let apiKey = "Your API Key"
     
-    private static func movieURL(endpoint: MovieEndpoint,parameters: [String:String]?) -> URL {
+
+    private static func movieURL(endpoint: MovieEndpoint, parameters: [String: String]?) -> URL? {
         
-        var components = URLComponents(string: baseMovieURLString+endpoint.rawValue)!
+        
+        guard var components = URLComponents(string: baseMovieURLString + endpoint.rawValue) else {
+            return nil
+        }
         
         var queryItems: [URLQueryItem] = []
         
-        let baseParams: [String:String] = [
-            "api_key":apiKey
+        let baseParams: [String: String] = [
+            "api_key": apiKey
         ]
         
-        for(key,value) in baseParams {
+        for (key, value) in baseParams {
             let item = URLQueryItem(name: key, value: value)
             queryItems.append(item)
         }
         
-        if let aditionalParameters = parameters {
-            for (key,value) in aditionalParameters {
+        if let additionalParameters = parameters {
+            for (key, value) in additionalParameters {
                 let item = URLQueryItem(name: key, value: value)
                 queryItems.append(item)
             }
@@ -38,48 +42,53 @@ struct MovieURL {
         
         components.queryItems = queryItems
         
-        return components.url!
+        guard let url = components.url else {
+            return nil
+        }
+        
+        return url
     }
+
     
     
     static var movieGenreListUrl: URL {
-        return movieURL(endpoint: .movieGenreList, parameters: nil)
+        return movieURL(endpoint: .movieGenreList, parameters: nil) ?? URL(string: "")!
     }
     
     static var popularListUrl: URL {
-        return movieURL(endpoint: .popular, parameters: nil)
+        return movieURL(endpoint: .popular, parameters: nil) ?? URL(string: "")!
     }
     
     static var topRatedListUrl: URL {
-        return movieURL(endpoint: .topRated, parameters: nil)
+        return movieURL(endpoint: .topRated, parameters: nil) ?? URL(string: "")!
     }
     
     static var upcomingListUrl: URL {
-        return movieURL(endpoint: .upcoming, parameters: nil)
+        return movieURL(endpoint: .upcoming, parameters: nil) ?? URL(string: "")!
     }
     
     static var nowPlayinListUrl: URL {
-        return movieURL(endpoint: .nowPlaying, parameters: ["release_date.lte":"2024-09-08","release_date.gte":"2024-08-08"])
+        return movieURL(endpoint: .nowPlaying, parameters: ["release_date.lte":"2024-09-08","release_date.gte":"2024-08-08"]) ?? URL(string: "")!
     }
     
     static func moviesWithGenreUrl(withGenre genre:String) -> URL {
-        return movieURL(endpoint: .movieWithGenre, parameters: ["with_genres" : genre])
+        return movieURL(endpoint: .movieWithGenre, parameters: ["with_genres" : genre]) ?? URL(string: "")!
     }
     
     static func movieSearchUrl(withQuery query:String) -> URL {
-        return movieURL(endpoint: .searchMovie, parameters: ["query" : query])
+        return movieURL(endpoint: .searchMovie, parameters: ["query" : query]) ?? URL(string: "")!
     }
     
     static func movieDetailUrl(withId movieId:String) -> URL {
-        return movieURL(endpoint: .details(movieId: movieId), parameters: nil)
+        return movieURL(endpoint: .details(movieId: movieId), parameters: nil) ?? URL(string: "")!
     }
     
     static func creditsUrl(withId movieId:String) -> URL {
-        return movieURL(endpoint: .credits(movieId: movieId), parameters: nil)
+        return movieURL(endpoint: .credits(movieId: movieId), parameters: nil) ?? URL(string: "")!
     }
     
     static func similar(withId movieId: String) -> URL {
-        return movieURL(endpoint: .similar(movieId: movieId), parameters: nil)
+        return movieURL(endpoint: .similar(movieId: movieId), parameters: nil) ?? URL(string: "")!
     }
     
     
