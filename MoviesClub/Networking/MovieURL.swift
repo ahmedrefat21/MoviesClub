b611a0eecf1735cc573139888de16c2f60b9c837
ahmedrefat21 <ahmedref21@gmail.com>
2024-08-27 14:51:08 +0300

    add optional values to MovieURL functions

--- MoviesClub/Networking/MovieURL.swift
+++ MoviesClub/Networking/MovieURL.swift
@@ -51,44 +51,44 @@ struct MovieURL {
 
     
     
-    static var movieGenreListUrl: URL {
-        return movieURL(endpoint: .movieGenreList, parameters: nil) ?? URL(string: "")!
+    static var movieGenreListUrl: URL? {
+        return movieURL(endpoint: .movieGenreList, parameters: nil)
     }
     
-    static var popularListUrl: URL {
-        return movieURL(endpoint: .popular, parameters: nil) ?? URL(string: "")!
+    static var popularListUrl: URL? {
+        return movieURL(endpoint: .popular, parameters: nil)
     }
     
-    static var topRatedListUrl: URL {
-        return movieURL(endpoint: .topRated, parameters: nil) ?? URL(string: "")!
+    static var topRatedListUrl: URL? {
+        return movieURL(endpoint: .topRated, parameters: nil)
     }
     
-    static var upcomingListUrl: URL {
-        return movieURL(endpoint: .upcoming, parameters: nil) ?? URL(string: "")!
+    static var upcomingListUrl: URL? {
+        return movieURL(endpoint: .upcoming, parameters: nil)
     }
     
-    static var nowPlayinListUrl: URL {
-        return movieURL(endpoint: .nowPlaying, parameters: ["release_date.lte":"2024-09-08","release_date.gte":"2024-08-08"]) ?? URL(string: "")!
+    static var nowPlayinListUrl: URL? {
+        return movieURL(endpoint: .nowPlaying, parameters: ["release_date.lte":"2024-09-08","release_date.gte":"2024-08-08"])
     }
     
-    static func moviesWithGenreUrl(withGenre genre:String) -> URL {
-        return movieURL(endpoint: .movieWithGenre, parameters: ["with_genres" : genre]) ?? URL(string: "")!
+    static func moviesWithGenreUrl(withGenre genre:String) -> URL? {
+        return movieURL(endpoint: .movieWithGenre, parameters: ["with_genres" : genre])
     }
     
-    static func movieSearchUrl(withQuery query:String) -> URL {
-        return movieURL(endpoint: .searchMovie, parameters: ["query" : query]) ?? URL(string: "")!
+    static func movieSearchUrl(withQuery query:String) -> URL? {
+        return movieURL(endpoint: .searchMovie, parameters: ["query" : query])
     }
     
-    static func movieDetailUrl(withId movieId:String) -> URL {
-        return movieURL(endpoint: .details(movieId: movieId), parameters: nil) ?? URL(string: "")!
+    static func movieDetailUrl(withId movieId:String) -> URL? {
+        return movieURL(endpoint: .details(movieId: movieId), parameters: nil)
     }
     
-    static func creditsUrl(withId movieId:String) -> URL {
-        return movieURL(endpoint: .credits(movieId: movieId), parameters: nil) ?? URL(string: "")!
+    static func creditsUrl(withId movieId:String) -> URL? {
+        return movieURL(endpoint: .credits(movieId: movieId), parameters: nil)
     }
     
-    static func similar(withId movieId: String) -> URL {
-        return movieURL(endpoint: .similar(movieId: movieId), parameters: nil) ?? URL(string: "")!
+    static func similar(withId movieId: String) -> URL? {
+        return movieURL(endpoint: .similar(movieId: movieId), parameters: nil)
     }
     
     
