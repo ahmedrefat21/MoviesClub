f69ecbbd64f6d5d0c9166962a7900de3ca285577
ahmedrefat21 <ahmedref21@gmail.com>
2024-08-27 17:08:56 +0300

    add cast list view

---
+++ MoviesClub/MovieDetail/View/CastsList.swift
@@ -0,0 +1,30 @@
+//
+//  CastsList.swift
+//  MoviesClub
+//
+//  Created by Ahmed Refat on 27/08/2024.
+//
+
+import SwiftUI
+
+struct CastsList: View {
+    //MARK: - PROPERTIES
+    var casts: [Casts]
+    
+    // MARK: - BODY
+    var body: some View {
+        VStack (alignment: .leading){
+            SubTitleView(title: "Cast")
+                .padding(.leading,10)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 0) {
+                    ForEach(casts,id:\.self) { cast in
+                        CastListItem(cast: cast)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
