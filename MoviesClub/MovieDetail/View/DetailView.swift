ec2645b193790d2a0b33810fde6fddeda37b199c
ahmedrefat21 <ahmedref21@gmail.com>
2024-08-27 18:44:21 +0300

    add DetailsView content

--- MoviesClub/MovieDetail/View/DetailView.swift
+++ MoviesClub/MovieDetail/View/DetailView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct DetailView: View {
     
     // MARK: - PROPERTIES
-    
+    @StateObject private var handler: DetailsViewHandler = .init()
     @Environment(\.presentationMode) private var presentationMode
     let movieId: Int
     
@@ -18,10 +18,10 @@ struct DetailView: View {
     var body: some View {
         VStack(spacing: 20) {
             detailsBar
-            
+            movieDetailContent
         }
         .background(BackgroundStyle.background)
-        
+        .onAppear(perform: loadData)
     }
     
     // MARK: - COMPONENTS
@@ -46,7 +46,33 @@ struct DetailView: View {
         }
     }
     
+    @ViewBuilder
+    private var movieDetailContent: some View {
+        if let movieDetail = handler.movieDetail {
+            ScrollView(.vertical, showsIndicators: false) {
+                PosterView(movieDetail: movieDetail)
+                    .overlay(
+                        DetailsHeader(movie: movieDetail)
+                            .offset(y: 130),
+                        alignment: .bottom
+                    )
+                
+                DescriptionView(movie: movieDetail)
+                    .padding(.top, 140)
+                CastsList(casts: handler.casts)
+                SimilarMoviesList(movies: handler.similarMovies)
+            }
+        }
+    }
     
+    // MARK: - METHODS
+    private func loadData() {
+        Task {
+            await handler.getMovieDetail(withId: movieId)
+            await handler.getSimilarMovies(withId: movieId)
+            await handler.getCasts(withId: movieId)
+        }
+    }
 }
 
 #Preview {
