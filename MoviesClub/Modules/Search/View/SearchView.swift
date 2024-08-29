364ca4c451ed3dedf4625cf1ce64b42beddd88a8
ahmedrefat21 <ahmedref21@gmail.com>
2024-08-29 15:01:19 +0300

    add main SearchView

---
+++ MoviesClub/Modules/Search/View/SearchView.swift
@@ -0,0 +1,81 @@
+//
+//  SearchView.swift
+//  MoviesClub
+//
+//  Created by Ahmed Refat on 29/08/2024.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    
+    // MARK: - Properties
+    @StateObject var handler: SearchViewHandler = .init()
+    @State private var searchTerm: String = ""
+    @State private var isComplete: Bool = false
+
+    // MARK: - BODY
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading, spacing: 5) {
+                TitleView(title: "Search", font: .largeTitle, addShadow: true)
+                    .padding(.horizontal, 10)
+
+                customSearchTextField
+
+                VStack {
+                    if !isComplete {
+                        Spacer()
+                        EmptyListView()
+                            .padding(.horizontal, 100)
+                        Spacer()
+                    } else {
+                        SearchMovieList(movies: handler.searchedMovies)
+                    }
+                }
+
+                Spacer()
+            }
+            .background(BackgroundStyle.background)
+            
+            
+        }
+    }
+
+    // MARK: - SearchTextField
+    private var customSearchTextField: some View {
+        TextField("Search", text: $searchTerm)
+            .onChange(of: searchTerm) { newValue in
+                Task {
+                    await handler.getMovies(query: newValue)
+                    withAnimation(.linear(duration: 1)) {
+                        isComplete = !newValue.isEmpty
+                    }
+                }
+            }
+            .font(.titleMedium)
+            .padding(.horizontal, 15)
+            .foregroundStyle(Color.textBase)
+            .frame(height: 45)
+            .background(RoundedRectangle(cornerRadius: 16)
+                .fill(Color.lightBackground.opacity(0.5))
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.textStrokeBase, lineWidth: 2)
+                ))
+            .textFieldStyle(DefaultTextFieldStyle())
+            .padding()
+            .overlay(
+                Image(systemName: "magnifyingglass")
+                    .font(.title2)
+                    .foregroundStyle(Color.textBase)
+                    .padding(.horizontal, 25),
+                alignment: .trailing
+            )
+    }
+
+}
+
+#Preview {
+    SearchView()
+}
